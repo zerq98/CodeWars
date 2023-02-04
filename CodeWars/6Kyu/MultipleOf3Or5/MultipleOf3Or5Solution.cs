bb9d11baa8d3b20c68340e686951b1a932493fd0
Mateusz Trybuła <zerq1998@gmail.com>
2023-02-04 23:17:17 +0100

    Add Solution to task Multiples of 3 or 5

---
+++ CodeWars/6Kyu/MultipleOf3Or5/MultipleOf3Or5Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars._6Kyu.MultipleOf3Or5
+{
+    public static class MultipleOf3Or5Solution
+    {
+        public static int Solution(int value)
+        {
+            int sum = 0;
+            if (value <= 0) return sum;
+
+            for (int i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
